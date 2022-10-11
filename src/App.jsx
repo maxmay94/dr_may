6b42903650bf382448b3d40b57ccd912fae386ec
maxmay94 <maxmay94@gmail.com>
2022-10-11 11:21:51 -0400

    add readme

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='bg-red-500'>Vite + React</h1>
+    <div>
+      <h1 className='bg-green-400 rounded-sm text-blue-600'>Dr. May Chiropractic</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
