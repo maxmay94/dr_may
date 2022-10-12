ad0a1b119f05d7349b180d8a42aaf185c621142f
maxmay94 <maxmay94@gmail.com>
2022-10-12 11:47:16 -0400

    add custom color

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -3,22 +3,27 @@ const NavBar = () => {
     <div className="flex justify-around bg-teal-900
     backdrop-blur-md shadow-md w-full
     fixed top-0 left-0 right-0 z-10">
-      <div className='text-amber-500 font-sans w-screen'>
+      <div className='font-sans w-screen'>
         <nav className="">
           <ul className="flex flex-row">
             <li className="grow m-2 text-sm font-light">
               <div className="text-left underline">
                 Email:
               </div>
-              docmay@gmail.com</li>
+              <p className="text-amber-500">
+                docmay@gmail.com
+              </p>
+            </li>
 
             <li className="grow m-2 mt-3 font-serif font-bold text-2xl text-slate-300">Dr.Peter May</li>
 
             <li className="m-2 text-sm font-light">
-              <div className="text-left underline">
+              <div className="text-right underline">
                 Phone
               </div>
-              (413)663-5264
+              <p className="text-amber-500">
+                (413)663-5264
+              </p>
             </li>
           </ul>
         </nav>
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,16 @@
 
 const Landing = () => {
   return (
-    <main className="flex text-center">
-      <div className="bg-teal-900 text-gray-300 m-5 p-2 drop">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nam dignissimos laborum adipisci! Commodi sapiente mollitia dignissimos numquam similique? Deserunt perspiciatis incidunt cupiditate libero commodi, adipisci autem corrupti dignissimos explicabo!
+    <main className="relative">
+      <div className="flex m-1">
+        <img className="w-2/3 rounded-l-md" src="/src/images/office_cover_photo.jpeg" alt="office"/>
+        <img className="w-1/3 rounded-r-md border-l-0 border-4 border-borderGreen" src="/src/images/serenity_dad.jpeg" alt="office"/>
+      </div>
+      <div className="flex m-5">
+        <img className="rounded-full m-1 w-20 h-20 drop-shadow" src="/src/images/baby_dad_head.jpeg" alt="baby_dad_head"/>
+        <div className="bg-teal-900 text-gray-300 m-1 p-2 drop-shadow rounded">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nam dignissimos laborum adipisci! Commodi sapiente mollitia dignissimos numquam similique? Deserunt perspiciatis incidunt cupiditate libero commodi, adipisci autem corrupti dignissimos explicabo!
+        </div>
       </div>
     </main>
   )
