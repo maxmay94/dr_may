6b89d8b419eb465c71bec1f33391c7c5af76201c
maxmay94 <maxmay94@gmail.com>
2022-12-12 13:10:49 -0500

    add FAQ

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,8 +4,12 @@ const Landing = () => {
     <main className="relative h-full">
       <div className="lg:w-2/3 mx-auto">
 
+        <div className="bg-teal-900/80 rounded p-2 drop-shadow  text-center m-2 mt-20 ">
+            Dr. May has been serving the North Adams community with expert chiropractic care for over 25 years. 
+        </div>
 
-        <div className="flex m-2 mt-20 ">
+
+        <div className="flex mt-10">
           <img className="w-2/3 rounded-l-md" src="/src/images/office_cover_photo.jpeg" alt="office"/>
           <img className="w-1/3 rounded-r-md border-l-0 border-4 lg:border-6 bg-borderGreen lg:p-1 lg:pl-0 border-borderGreen" src="/src/images/serenity_dad.jpeg" alt="office"/>
         </div>
@@ -20,39 +24,114 @@ const Landing = () => {
           </div>
 
           <br />
-          <div className="bg-emerald-900/80 rounded p-2 drop-shadow">
-            Dr. May has been serving the North Adams community with expert chiropractic care for over 25 years. 
-            <br />
-            <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora consequatur odit nobis, cupiditate consequuntur quia expedita sint eius dignissimos tempore vitae iure ratione molestias ipsam cum unde vero delectus? Nostrum.
+          <div className="bg-emerald-900/80 rounded p-2 drop-shadow text-center">
+            In my 36+ years as a chiropractor I have helped 1000&#39;s of patients in our community
+            with problems ranging from routine neck and back pain, to “pinched nerves”, headaches, a
+            variety of arm-shoulder-wrist pain and hip-knee-ankle-foot pain, and more. Every
+            complaint is unique to the individual. I will do everything I can to help you. Most importantly, I
+            will give you excellent advice and home instructions on how to help yourself get better faster.
+            And, if you are interested, I will go beyond that and give you vitally important information,
+            instruction, and advice on how to improve your overall health.
           </div>
         </div>
 
         <br />
 
+        <p className="text-center text-2xl m-5">
+          Frequently Asked Questions
+        </p>
         <div className="m-5 bg-emerald-900/30 p-5 rounded drop-shadow">
 
-          <div className="flex ">
-            <img className="rounded-full m-5 w-20 h-20 drop-shadow hover:animate-spin" src="/src/images/baby_dad_head.jpeg" alt="baby_dad_head"/>
-            <div className="bg-teal-900 text-gray-300 m-1 p-2 drop-shadow rounded">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nam dignissimos laborum adipisci! Commodi sapiente mollitia dignissimos numquam similique? Deserunt perspiciatis incidunt cupiditate libero commodi, adipisci autem corrupti dignissimos explicabo!
+          {/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+
+          <div className="flex">
+            <img className="rounded-full w-20 h-20 drop-shadow hover:animate-spin" src="/src/images/max_bush.jpeg" alt="max_behind_bush" />
+            <div className="bg-teal-900 text-gray-300 m-1 my-auto p-2 drop-shadow rounded">
+              Question: Are you taking new patients? And how far out are you booking?
             </div>
+          </div>
+
+          <div className="flex mt-2">
+            <div className="bg-slate-300 text-teal-900 m-1 my-auto p-2 drop-shadow rounded">
+              Yes, I am accepting new patients. And, I can usually see them within a few days after
+              their call. Sometimes, the same day. My hours are, by appointment.
+            </div>
+            <img className="rounded-full m-5 w-20 h-20 drop-shadow hover:animate-spin" src="/src/images/baby_dad_head.jpeg" alt="baby_dad_head"/>
           </div>
 
           <br />
 
+          {/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+
+
           <div className="flex">
-            <img className="rounded-full m-5 w-20 h-20 drop-shadow hover:animate-spin" src="/src/images/dad_headshot.jpeg" alt="normal_dad_head"/>
-            <div className="bg-teal-900 text-gray-300 m-0 p-2 drop-shadow rounded">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nam dignissimos laborum adipisci! Commodi sapiente mollitia dignissimos numquam similique? Deserunt perspiciatis incidunt cupiditate libero commodi, adipisci autem corrupti dignissimos explicabo!
+            <img className="rounded-full w-20 h-20 drop-shadow hover:animate-spin" src="/src/images/max_cool.jpg" alt="max_2_cool_4_cool" />
+            <div className="bg-teal-900 text-gray-300 m-1 my-auto p-2 drop-shadow rounded">
+              Question: Do you take my health insurance?
             </div>
           </div>
 
+          <div className="flex mt-2">
+            <div className="bg-slate-300 text-teal-900 m-0 my-auto p-2 drop-shadow rounded">
+              Currently, I am only accepting BC/BS of Massachusetts and Medicare. I am not on
+              other provider panels. You still have insurance. But, you will have to pay as you go, submit
+              the claim yourself, and accept whatever they reimburse you. You will need to discuss this
+              with your insurance company to find out their reimbursement for “out-of-network
+              providers.”
+              Whatever insurance you have, you are always responsible for deductibles and co-
+              pays. For most patients with insurance, this means they are paying close to my self-pays
+              fees anyway.
+              Note: your insurance doesn&#39;t pay for your electrician, plumber, or even your hair and
+              nail appointments, etc. But, you still use them and pay for them, when needed. Isn&#39;t your
+              health as, or more, important?
+            </div>
+            <img className="rounded-full m-5 w-20 h-20 drop-shadow hover:animate-spin" src="/src/images/dad_headshot.jpeg" alt="normal_dad_head"/>
+          </div>
+
+                    {/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+
+
+                    <div className="flex">
+            <img className="rounded-full w-20 h-20 drop-shadow hover:animate-spin" src="/src/images/max_jeep.jpg" alt="max_jeep" />
+            <div className="bg-teal-900 text-gray-300 m-1 my-auto p-2 drop-shadow rounded">
+              Question: How much is a visit?
+            </div>
+          </div>
+
+          <div className="flex mt-2">
+            <div className="bg-slate-300 text-teal-900 m-0 my-auto p-2 drop-shadow rounded">
+              On the initial office visit we go over the requisite paperwork. Then, we go into the room,
+              do a history and exam. Based on that, I will give you a diagnosis that you will understand.
+              Next, and most importantly, I will give you explicit verbal and written home instructions to
+              expedite healing. Then, I will explain what I am going to do and YES, I DO treat on the
+              first visit. All of that takes about 1 hour. $125.00 Subsequent visits are $50.00
+
+            </div>
+            <img className="rounded-full m-5 w-20 h-20 drop-shadow hover:animate-spin" src="/src/images/serenity_dad_head.jpeg" alt="normal_dad_head"/>
+          </div>
+          <div className="flex mt-2">
+            <div className="bg-slate-300 text-teal-900 m-0 my-auto p-2 drop-shadow rounded">
+              <div className="text-orange-700">
+                Note for Medicare patients:
+              </div>
+              Medicare and your supplemental insurance DO NOT COVER the initial exam. You are
+              responsible for the $125.00 fee. On the initial, and on subsequent, visits Medicare
+              covers 80% of the treatment cost and your supplemental insurance, if you have it, will
+              probably cover the remaining 20%.
+            </div>
+            <img className="rounded-full m-5 w-20 h-20 drop-shadow hover:animate-spin" src="/src/images/dad_black_eye.jpeg" alt="normal_dad_head"/>
+          </div>
         </div>
+
+        
+
+        
       
         <div className="flex mx-5 my-10 pb-10 drop-shadow">
           <marquee className="text-3xl rounded text-slate-200 bg-emerald-900/80" direction="right">
-            (413) 664-9050 Call Today!
+            <div className="animate-pulse">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nam dignissimos laborum adipisci! Commodi sapiente mollitia dignissimos numquam similique? Deserunt perspiciatis incidunt cupiditate libero commodi, adipisci autem corrupti dignissimos explicabo!
+            </div>
           </marquee>
         </div>
       </div>
